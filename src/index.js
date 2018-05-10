dcd6005eaba32f7bb060fd32eda1103b34228c0f
KU Lok Sun <sundaykuloksun@gmail.com>
2018-05-10 17:58:31 +0800

    Add test case on setting populated object in host object

--- src/index.js
+++ src/index.js
@@ -36,6 +36,20 @@ describe('populate()', () => {
     expect(story.author.name).to.be.equal('Ian Fleming');
   });
 
+  // Conclusion: don't try to update populated object by set() from host object;
+  context('when we update populated object author by story.set({ author: { ... } })', () => {
+    it('will create a new populated object', async () => {
+      const story = await Story
+        .findOne({ title: 'Casino Royale' })
+        .populate('author')
+        .exec();
+      const originAuthorId = story.author._id;
+      story.set({ author: {name: 'Sunday'} });
+      // A new object is created here
+      expect(story.author._id).to.be.not.equal(originAuthorId);
+    });
+  });
+
   afterEach(async () => {
     await Person.remove();
     await Story.remove();
