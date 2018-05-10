e03f9215e693c304db08e4333ad292612bc2b373
KU Lok Sun <sundaykuloksun@gmail.com>
2018-05-10 16:08:34 +0800

    Set up model with ref

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 const mongoose = require('mongoose');
+const {
+  initPersonModel,
+  initStoryModel
+} = require('./models.js');
 const { main: subdoc } = require('./subdoc.js');
 
 const main = async () => {
   let ret;
   mongoose.set('debug', true);
   mongoose.connect('mongodb://mongo/test');
-  await subdoc();
+
+  const Person = initPersonModel(mongoose);
+  const Story = initStoryModel(mongoose);
+
+  const author = new Person({
+    name: 'Ian Fleming',
+    age: 50
+  });
+
+  await author.save();
+
+  const story = new Story({
+    title: 'Casino Royale',
+    // assign the _id from the person
+    author: author._id
+  });
+
+  await story.save();
+
+
   mongoose.disconnect();
 }
 
