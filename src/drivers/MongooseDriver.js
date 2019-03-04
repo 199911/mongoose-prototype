dcb4f58ee0bb424db45d2edfc3be1d1424af7529
Ku Lok Sun <sundaykuloksun@gmail.com>
2019-03-05 00:15:16 +0800

    Implement new mongoose driver

---
+++ src/drivers/MongooseDriver.js
@@ -0,0 +1,38 @@
+const { Mongoose } = require('mongoose');
+const { MongoClient } = require('mongodb');
+
+class MongooseDriver {
+
+  constructor(uri, name) {
+    this.config = { uri, name };
+  }
+
+  async connectAsync() {
+    const { uri, name } = this.config;
+    this.mongoose = new Mongoose();
+    await this.mongoose.connect(
+      uri,
+      {
+        keepAlive: true,
+        dbName: name,
+        useNewUrlParser: true,
+      },
+    );
+    return this.mongoose;
+  }
+
+  async disconnectAsync() {
+    return this.mongoose.disconnect();
+  }
+
+  async resetAsync() {
+    const { uri, name } = this.config;
+    const connection = await MongoClient.connect(uri, { useNewUrlParser: true });
+    const db = await connection.db(name);
+    await db.dropDatabase();
+    await connection.close();
+  }
+}
+
+
+module.exports = MongooseDriver;
