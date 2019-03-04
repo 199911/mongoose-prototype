3eabb5df2b32358975a5462275fd3cf3cbcedf4a
KU Lok Sun <sundaykuloksun@gmail.com>
2019-03-04 11:49:40 +0800

    Implement basic mongoose test
    For testing the new mongoose driver.

---
+++ src/basic.js
@@ -0,0 +1,49 @@
+const { assert } = require('chai');
+
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const { connectAsync, disconnectAsync } = require('./drivers/mongoose.js');
+
+let mongoMemoryServer;
+let mongoose;
+
+before('Setup Mock MongoDB', async () => {
+  mongoMemoryServer = new MongoMemoryServer({
+    autoStart: false,
+  });
+  await mongoMemoryServer.start();
+});
+
+after('Teardown Mock MongoDB', async () => {
+  mongoMemoryServer.stop();
+});
+
+describe('Basic test', () => {
+  beforeEach('Setup mongoose', async () => {
+    const uri = await mongoMemoryServer.getConnectionString();
+    const name = 'jest';
+    mongoose = await connectAsync({ uri, name });
+  });
+
+  afterEach('Tear down mongoose', async () => {
+    await disconnectAsync(mongoose);
+  });
+
+  let Kitten;
+  const fixture = { name: 'Silence' };
+
+  beforeEach('Setup models', () => {
+    const kittySchema = new mongoose.Schema({
+      name: String
+    });
+    Kitten = mongoose.model('Kitten', kittySchema);
+  });
+
+  beforeEach('Setup kitten', async () => {
+    silence = await Kitten.create(fixture);
+  });
+
+  it('should find a kitten name slience', async () => {
+    const kitten = await Kitten.findOne(fixture);
+    assert.include(kitten, fixture);
+  });
+});
