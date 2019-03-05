38c5eb9161fc53349c42865d2e22057e718d4fcc
Ku Lok Sun <sundaykuloksun@gmail.com>
2019-03-06 00:15:37 +0800

    Update bulkwrite

--- src/bulkWrite.js
+++ src/bulkWrite.js
@@ -1,242 +1,218 @@
-const { Schema } = require('mongoose');
 const { assert } = require('chai');
 
-const { connect } = require('./drivers/mongoose.js');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const MongooseDriver = require('./drivers/MongooseDriver.js');
 
-DEBUG = true;
+let mongoMemoryServer;
+let mongoose;
+let mongooseDriver;
 
-const item = new Schema({
-  a: Number,
-  b: Number,
-  c: Number,
-  doc: {
-    e: Number,
-    f: Number,
-    g: Number,
-  }
+before('Setup Mock MongoDB', async () => {
+  mongoMemoryServer = new MongoMemoryServer({
+    autoStart: false,
+  });
+  await mongoMemoryServer.start();
 });
 
-describe('bulkWrite() with updateOne upsert operation', () => {
-  let conn, Item;
+after('Teardown Mock MongoDB', async () => {
+  mongoMemoryServer.stop();
+});
+
+beforeEach('Setup mongoose', async () => {
+  const uri = await mongoMemoryServer.getConnectionString();
+  const name = 'jest';
+  mongooseDriver = new MongooseDriver(uri, name);
+  mongoose = await mongooseDriver.connectAsync();
+});
+
+afterEach('Tear down mongoose', async () => {
+  await mongooseDriver.resetAsync();
+  await mongooseDriver.disconnectAsync();
+});
+
+describe('bulkWrite()', () => {
+  let Item;
 
   beforeEach(() => {
-    conn = connect();
-    Item = conn.model('Item', item);
-  });
-
-  afterEach(async () => {
-    await Item.remove();
-    conn.close();
+    const itemSchema = new mongoose.Schema({
+      a: Number,
+      b: Number,
+      c: Number,
+      doc: {
+        e: Number,
+        f: Number,
+        g: Number,
+      },
+    });
+    Item = mongoose.model('Item', itemSchema);
   });
 
   context('when item exists', () => {
+    const fixture = {
+      a: 1,
+      b: 2,
+      c: 3,
+      doc: {
+        e: 4,
+        f: 5,
+      },
+    };
 
-    beforeEach(async () => {
-      const i = new Item({
-        a: 1,
-        b: 2,
-        c: 3,
-      })
-      await i.save();
-    })
+    let itemId;
 
-    it('should update the item', async () => {
-      const expect = { a:1, b:4, c:6 };
-      const before = await Item.findOne({a:1}).lean();
-      const {
-        insertedCount,
-        matchedCount,
-        modifiedCount,
-        deletedCount,
-        upsertedCount,
-        upsertedIds,
-        insertedIds,
-      } = await Item.bulkWrite([
-        {
-          updateOne: {
-            filter: { a: 1 },
-            update: {
-              b: 4,
-              c: 6,
-            },
-            upsert: true,
-          }
-        }
-      ]);
-      const after = await Item.findOne({a:1}).lean();
-      assert.ownInclude(after, expect)
-      if (DEBUG) {
-        console.log({
-          insertedCount,
-          matchedCount,
-          modifiedCount,
-          deletedCount,
-          upsertedCount,
-          upsertedIds,
-          insertedIds,
-        });
-        console.log(before);
-        console.log(after);
-        console.log(expect);
-      }
+    beforeEach('Setup item', async () => {
+      const item = new Item(fixture);
+      await item.save();
+      itemId = item._id;
     });
 
-  });
+    context('when update some fields with updateOne upsert operation', () => {
+      const expect = { a: 2, b: 4, c: 6 };
+      let ret;
 
-  context('when update partial item', () => {
-    beforeEach(async () => {
-      const i = new Item({
-        a: 1,
-        b: 2,
-        c: 3,
-      })
-      await i.save();
-    })
-
-    it('should update item and keep other fields unchanged', async () => {
-      const expect = { a:1, b:4 };
-      const before = await Item.findOne({a:1}).lean();
-      const {
-        insertedCount,
-        matchedCount,
-        modifiedCount,
-        deletedCount,
-        upsertedCount,
-        upsertedIds,
-        insertedIds,
-      } = await Item.bulkWrite([
-        {
-          updateOne: {
-            filter: { a: 1 },
-            update: {
-              b: 4,
+      beforeEach('Run query', async () => {
+        ret = await Item.bulkWrite([
+          {
+            updateOne: {
+              filter: { _id: itemId },
+              update: expect,
+              upsert: true,
             },
-            upsert: true,
-          }
-        }
-      ]);
-      const after = await Item.findOne({a:1}).lean();
-      if (DEBUG) {
-        console.log({
-          insertedCount,
-          matchedCount,
-          modifiedCount,
-          deletedCount,
-          upsertedCount,
-          upsertedIds,
-          insertedIds,
-        });
-        console.log(before);
-        console.log(after);
-        console.log(expect);
-      }
-      assert.ownInclude(after, expect);
-    });
-  });
+          },
+        ]);
+      });
 
-  context('when item does not exists', () => {
-    it('should update item and keep other fields unchanged', async () => {
-      const expect = { a: 3, b:6 };
-      const before = await Item.findOne({a:3}).lean();
-      const res = await Item.bulkWrite([
-        {
-          updateOne: {
-            filter: { a: 3 },
-            update: {
-              b: 6,
+      it('should update the item', async () => {
+        const item = await Item.findById(itemId).lean();
+        assert.ownInclude(item, expect);
+      });
+
+      it('should return query result object', async () => {
+        const expectedRet = {
+          result: {
+            ok: 1,
+            writeErrors: [],
+            writeConcernErrors: [],
+            insertedIds: [],
+            nInserted: 0,
+            nUpserted: 0,
+            nMatched: 1,
+            nModified: 1,
+            nRemoved: 0,
+            upserted: [],
+          },
+          insertedCount: 0,
+          matchedCount: 1,
+          modifiedCount: 1,
+          deletedCount: 0,
+          upsertedCount: 0,
+          upsertedIds: {},
+          insertedIds: {},
+          n: 0,
+        };
+        assert.deepOwnInclude(ret, expectedRet);
+      });
+    });
+
+    context('when update one field with updateOne upsert operation', () => {
+      const update = { b: 4 };
+      let ret;
+
+      beforeEach('Run query', async () => {
+        ret = await Item.bulkWrite([
+          {
+            updateOne: {
+              filter: { _id: itemId },
+              update,
+              upsert: true,
             },
-            upsert: true,
-          }
-        }
-      ]);
-      const {
-        insertedCount,
-        matchedCount,
-        modifiedCount,
-        deletedCount,
-        upsertedCount,
-        upsertedIds,
-        insertedIds,
-      } = res;
-      const after = await Item.findOne({a:3}).lean();
-      if (DEBUG) {
-        console.log({
-          insertedCount,
-          matchedCount,
-          modifiedCount,
-          deletedCount,
-          upsertedCount,
-          upsertedIds,
-          insertedIds,
-        });
-        console.log(before);
-        console.log(after);
-        console.log(expect);
-      }
-      assert.ownInclude(after, expect);
-    });
-  });
+          },
+        ]);
+      });
 
-  context('when update partial sub-document in item', () => {
-    beforeEach(async () => {
-      const i = new Item({
-        a: 999,
+      it('should update one fields and keep other fields unchanged', async () => {
+        const expect = { a: 1, b: 4, c: 3 };
+        const item = await Item.findById(itemId).lean();
+        assert.ownInclude(item, expect);
+      });
+
+      it('should match and modify 1 document', async () => {
+        const expectedRet = {
+          matchedCount: 1,
+          modifiedCount: 1,
+        };
+        assert.equal(ret.matchedCount, 1);
+        assert.equal(ret.modifiedCount, 1);
+        assert.ownInclude(ret.result, {
+          ok: 1,
+          nMatched: 1,
+          nModified: 1,
+        });
+      });
+    });
+
+    context('when update object with updateOne upsert operation', () => {
+      const update = {
         doc: {
           e: 6,
-          f: 8,
-          g: 9,
-        }
-      })
-      await i.save();
-    })
-
-    it('should update sub-document and keep other fields unchanged', async () => {
-      const expect = {
-        a: 999,
-        doc: {
-          e: 6,
-          f: 6,
           g: 6,
-        }
+        },
       };
-      const before = await Item.findOne({a: 999}).lean();
-      const {
-        insertedCount,
-        matchedCount,
-        modifiedCount,
-        deletedCount,
-        upsertedCount,
-        upsertedIds,
-        insertedIds,
-      } = await Item.bulkWrite([
-        {
-          updateOne: {
-            filter: { a: 999 },
-            update: {
-              'doc.f': 6,
-              'doc.g': 6,
+      let ret;
+
+      beforeEach('Run query', async () => {
+        ret = await Item.bulkWrite([
+          {
+            updateOne: {
+              filter: { _id: itemId },
+              update,
+              upsert: true,
             },
-            upsert: true,
-          }
-        }
-      ]);
-      const after = await Item.findOne({a: 999}).lean();
-      if (DEBUG) {
-        console.log({
-          insertedCount,
-          matchedCount,
-          modifiedCount,
-          deletedCount,
-          upsertedCount,
-          upsertedIds,
-          insertedIds,
-        });
-        console.log(before);
-        console.log(after);
-        console.log(expect);
-      }
-      assert.deepOwnInclude(after, expect);
+          },
+        ]);
+      });
+
+      it('should replace the whole object and keep other fields unchanged', async () => {
+        const expect = {
+          a: 1,
+          b: 2,
+          c: 3,
+          doc: {
+            e: 6,
+            // f: 5,
+            g: 6,
+          },
+        };
+        const item = await Item.findById(itemId).lean();
+        assert.deepOwnInclude(item, expect);
+        // item.doc.f is removed}
+        // whole field `item.doc` is updates, the object is replaced by { e:6, g:6 }
+        assert.notExists(item.doc.f);
+      });
     });
   });
 
+  context('when item does not exist', () => {
+    context('when update some fields with updateOne upsert operation', () => {
+      const filter = { a: 3 };
+      const update = { a: 3, b: 6 };
+
+      beforeEach('Run query', async () => {
+        const res = await Item.bulkWrite([
+          {
+            updateOne: {
+              filter,
+              update,
+              upsert: true,
+            },
+          },
+        ]);
+      });
+
+      it('should create item', async () => {
+        const item = await Item.findOne(filter).lean();
+        assert.ownInclude(item, update);
+      });
+    });
+  });
 });
