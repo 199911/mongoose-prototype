4e77db02c1f7ab0b929bf19744f21af734c5e939
Ku Lok Sun <sundaykuloksun@gmail.com>
2019-03-06 01:05:33 +0800

    Update some of the test in sub documents

--- src/subdoc.js
+++ src/subdoc.js
@@ -1,101 +1,143 @@
-const { expect } = require('chai');
+const { assert } = require('chai');
 
-const { connect } = require('./drivers/mongoose.js');
-const {
-  initParentModel
-} = require('./models.js');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const MongooseDriver = require('./drivers/MongooseDriver.js');
+
+let mongoMemoryServer;
+let mongoose;
+let mongooseDriver;
+
+before('Setup Mock MongoDB', async () => {
+  mongoMemoryServer = new MongoMemoryServer({
+    autoStart: false,
+  });
+  await mongoMemoryServer.start();
+});
+
+after('Teardown Mock MongoDB', async () => {
+  mongoMemoryServer.stop();
+});
+
+beforeEach('Setup mongoose', async () => {
+  const uri = await mongoMemoryServer.getConnectionString();
+  const name = 'jest';
+  mongooseDriver = new MongooseDriver(uri, name);
+  mongoose = await mongooseDriver.connectAsync();
+});
+
+afterEach('Tear down mongoose', async () => {
+  await mongooseDriver.resetAsync();
+  await mongooseDriver.disconnectAsync();
+});
 
 describe('sub-documents', () => {
-  let conn;
   let Parent;
   let parent;
-  let originalChildId;
-  beforeEach(async () => {
-    conn = connect();
-    Parent = initParentModel(conn);
+  let childId;
 
+  beforeEach('Setup model', async () => {
+    const childSchema = new mongoose.Schema({
+      name: 'string',
+      age: Number,
+    });
+    const parentSchema = new mongoose.Schema({
+      name: String,
+      age: Number,
+      children: [childSchema],
+      child: childSchema,
+    });
+    Parent = mongoose.model('Parent', parentSchema);
+  });
+
+  beforeEach('Setup document', async () => {
     parent = new Parent({
       name: 'Mum',
       children: [
         {
-          name: 'Matt' ,
-          age: 22
+          name: 'Matt',
+          age: 22,
         },
         {
           name: 'Sarah',
-          age: 18
-        }
+          age: 18,
+        },
       ],
       child: {
         name: 'Matt',
-        age: 22
-      }
+        age: 22,
+      },
     });
     await parent.save();
-    originalChildId = parent.child._id.toString();
+    childId = parent.child._id;
   });
 
   // We can update sub document with path1.path2
-  it('should update child with parent.set("child.name", value)', async () => {
-    parent.set('child.name', 'Sunday');
-    await parent.save();
+  context('when update child with dot notation (child.name)', () => {
+    it('should update child name only', async () => {
+      parent.set('child.name', 'Sunday');
+      await parent.save();
 
-    const myParent = await Parent
-      .findOne({ name: 'Mum' })
-      .exec();
-
-    // No new child is created
-    expect(myParent.child._id.toString()).to.be.equal(originalChildId);
-    expect(myParent.child.name).to.be.equal('Sunday');
-    expect(myParent.child.age).to.be.equal(22);
+      const myParent = await Parent
+        .findOne({ name: 'Mum' })
+        .lean()
+        .exec();
+      // No new child is created
+      assert.equal(myParent.child._id.toString(), childId.toString());
+      assert.ownInclude(myParent.child, { name: 'Sunday', age: 22 });
+    });
   });
 
   // We can update sub document with path1.path2, but the other field in child got removed
-  it('should update child with parent.set({ child: {name: value} })', async () => {
-    parent.set({ child: {name: 'Sunday'} });
-    await parent.save();
+  context('when update child with object notation (child: { name })', () => {
+    it('should replace the whole child object', async () => {
+      parent.set({ child: { name: 'Sunday' } });
+      await parent.save();
 
-    const myParent = await Parent
-      .findOne({ name: 'Mum' })
-      .exec();
+      const myParent = await Parent
+        .findOne({ name: 'Mum' })
+        .lean()
+        .exec();
 
-    // New sub-document will be created, as we set an object to sub-document
-    expect(myParent.child._id.toString()).to.be.not.equal(originalChildId);
-    expect(myParent.child.name).to.be.equal('Sunday');
-    expect(myParent.child.age).to.be.undefined;
+      // Note: New sub-document will be created, as we set an object to sub-document
+      // Id is different as it is a new object
+      assert.notEqual(myParent.child._id.toString(), childId.toString());
+      assert.ownInclude(myParent.child, { name: 'Sunday' });
+      // age is undefined, as the whold child object is replaced
+      assert.isUndefined(myParent.child.age);
+    });
   });
 
   // We have to call parent.save() to update sub-document
-  it('will not update parent.child if call child.save()', async () => {
-    const child = parent.child;
-    child.set({name: 'Sunday'});
+  it.skip('will not update parent.child if call child.save()', async () => {
+    const { child } = parent;
+    child.set({ name: 'Sunday' });
     await child.save();
 
     const myParent = await Parent
       .findOne({ name: 'Mum' })
       .exec();
 
-    expect(myParent.child._id.toString()).to.be.equal(originalChildId);
-    expect(myParent.child.name).to.be.equal('Matt');
+    assert(myParent.child._id.toString()).to.be.equal(childId);
+    assert(myParent.child.name).to.be.equal('Matt');
   });
 
   // child.set will mark the field dirty, so parent.ave() will update the record
-  it('will update parent.child if call child.set() and parent.save()', async () => {
-    const child = parent.child;
-    child.set({name: 'Sunday'});
+  it.skip('will update parent.child if call child.set() and parent.save()', async () => {
+    const { child } = parent;
+    child.set({ name: 'Sunday' });
     await parent.save();
 
     const myParent = await Parent
       .findOne({ name: 'Mum' })
       .exec();
 
-    expect(myParent.child._id.toString()).to.be.equal(originalChildId);
-    expect(myParent.child.name).to.be.equal('Sunday');
-    expect(myParent.child.age).to.be.equal(22);
+    assert(myParent.child._id.toString()).to.be.equal(childId);
+    assert(myParent.child.name).to.be.equal('Sunday');
+    assert(myParent.child.age).to.be.equal(22);
   });
 
   // We can update sub document in array with path1.index
-  it('should update child with parent.set("children.1.name", value)', async () => {
+  it.skip('should update child with parent.set("children.1.name", value)', async () => {
     parent.set('children.1.name', 'Sunday');
     await parent.save();
 
@@ -103,12 +145,7 @@ describe('sub-documents', () => {
       .findOne({ name: 'Mum' })
       .exec();
 
-    expect(myParent.children[1].name).to.be.equal('Sunday');
-    expect(myParent.children[1].age).to.be.equal(18);
+    assert(myParent.children[1].name).to.be.equal('Sunday');
+    assert(myParent.children[1].age).to.be.equal(18);
   });
-
-  afterEach(async () => {
-    await Parent.remove();
-    conn.close();
-  })
 });
