5d441a9982eb540e2ff5883d8362c39e0b10e0c2
KU Lok Sun <sundaykuloksun@gmail.com>
2019-03-22 12:18:52 +0800

    Study the ObjectId object return from mongoose query

--- test/SchemaType-Types.js
+++ test/SchemaType-Types.js
@@ -40,10 +40,33 @@ describe('SchemaType vs Type', () => {
   });
 
   it('Schema.Types.ObjectId is different from Types.ObjectId', () => {
-    assert.notEqual(SchemaTypeObjectId, TypeObjectId)
+    assert.notEqual(SchemaTypeObjectId, TypeObjectId);
   });
 
   it('Instance ObjectId is Types.ObjectId', () => {
-    assert.notEqual(mongoose.ObjectId, TypeObjectId)
+    assert.notEqual(mongoose.ObjectId, TypeObjectId);
+  });
+
+  describe('when we have a document', () => {
+    let Kitten;
+    let inMemoryDoc;
+    beforeEach('Setup document', async () => {
+      const kittySchema = new mongoose.Schema({
+        name: String,
+      });
+      Kitten = mongoose.model('Kitten', kittySchema);
+      inMemoryDoc = await Kitten.create({ name: 'slience' });
+    });
+
+    it('id in memory doc is Types.ObjectId', async () => {
+      assert.instanceOf(inMemoryDoc._id, TypeObjectId);
+      assert.notInstanceOf(inMemoryDoc._id, SchemaTypeObjectId);
+    });
+
+    it('id of doc return from db is Types.ObjectId', async () => {
+      const doc = await Kitten.findOne({});
+      assert.instanceOf(doc._id, TypeObjectId);
+      assert.notInstanceOf(doc._id, SchemaTypeObjectId);
+    });
   });
 });
